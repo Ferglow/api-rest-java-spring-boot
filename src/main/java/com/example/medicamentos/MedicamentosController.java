bba116bd31e0243c45ad544733e37f9147325fa0
Ferglow <fernando.a.h@outlook.com>
2025-07-14 12:34:29 -0600

    getter and setters

--- src/main/java/com/example/medicamentos/MedicamentosController.java
+++ src/main/java/com/example/medicamentos/MedicamentosController.java
@@ -1,8 +1,17 @@
 package com.example.medicamentos;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class MedicamentosController {
+
+
+
+
+    @GetMapping("/")
+    public String prueba(){
+        return  "Hola Fernando";
+    }
 
 }
